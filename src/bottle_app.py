e61e6c13a26f46a673fd502bcc16a2dd020aeb43
carlotolla <cetoli@gmail.com>
2020-07-16 16:41:29 -0300

    Adiciona uma nova rota no app

--- src/bottle_app.py
+++ src/bottle_app.py
@@ -1,10 +1,30 @@
 # -*- coding: utf-8 -*-
-# A very simple Bottle Hello World app for you to get started with...
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Tutorial Um - respondendo dúvidas do SuperPython.
+
+.. codeauthor:: Carlo Oliveira <carlo@ufrj.br>
+
+- Como associar um evento a uma imagem
+- Como combinar cenas em salas diferentes
+- Como capturar o teclado
+
+Sem Classes neste modulo:
+
+Changelog
+---------
+.. versionadded::    20.07
+        Adiciona o gerenciador de chamadas http via bottle.
+
+"""
 from bottle import default_app, route
 
 @route('/')
 def hello_world():
     return 'Tutorial Dois - aprendendo Git e Bottle'
+
+@route('/oi')
+def oi_mundo():
+    return 'Tutorial Dois - ensaiando uma nova rota'
 
 application = default_app()
 
